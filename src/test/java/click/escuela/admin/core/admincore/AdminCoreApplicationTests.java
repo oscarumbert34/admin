e970235e5c31f82fd73bb5703f892c6e98ad0fd7
umbert oscar <oscar.umbert@gmail.com>
2021-04-20 19:00:02 -0300

    se arreglan test

--- src/test/java/click/escuela/admin/core/admincore/AdminCoreApplicationTests.java
+++ src/test/java/click/escuela/admin/core/admincore/AdminCoreApplicationTests.java
@@ -1,16 +1,9 @@
 package click.escuela.admin.core.admincore;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest
 class AdminCoreApplicationTests {
-	@Before
-	public void setup() {
-		
-	}
+
 	
 	@Test
 	void contextLoads() {
