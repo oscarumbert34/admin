9b15846bc6e19176f56effaa9c51c2dea9ab07b5
Evelin Aragon <eve_aragon@hotmail.com>
2021-04-24 11:49:45 -0300

    prueba sonar5

--- src/main/java/click/escuela/admin/core/api/CourseApi.java
+++ src/main/java/click/escuela/admin/core/api/CourseApi.java
@@ -3,7 +3,7 @@ package click.escuela.admin.core.api;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-
+	//esto es un comentario
 @Getter
 @Setter
 public class CourseApi {
@@ -11,5 +11,6 @@ public class CourseApi {
 	private String name;
 	private List<String> students;
 	//esto es un comentario
-	private String name2;
+	public String name2;
+	public String name3;
 }
