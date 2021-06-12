9860e5b8a82bcfecdf12121d59bc06f48a4cc15f
umbert oscar <oscar.umbert@gmail.com>
2021-06-12 09:29:36 -0300

    import unused was removed

--- src/main/java/click/escuela/admin/core/feign/StudentController.java
+++ src/main/java/click/escuela/admin/core/feign/StudentController.java
@@ -2,7 +2,6 @@ package click.escuela.admin.core.feign;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
