37505cb3e969f1f37b0ae93ffda8d054399b6cb0
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-06-17 22:01:58 -0300

    Develop (#26)
    * It configurated all to create Teacher (#13)
    
    * It configurated all to create Teacher
    
    * All the tests were created. And the packages are organized by each type
    
    * the url student was modified
    
    * the url student was modified
    
    * the database was changed to H2
    
    * Update teacher was created and all its tests (#15)
    
    * the swagger configuration was added (#18)
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    
    * feature/CE-265-listar-profesores (#20)
    
    * Get Teacher By Id and, Get Teachers by School Id and Course Id; were implented
    
    * All the tests were created
    
    * Code Smell fixed
    
    * Code Smell fixed
    
    * Another code Smell fixed
    
    * jacoco
    
    * the test was fixed
    
    * Get Bills by some params were implemented and also their tests (#21)
    
    * Entity School and its repository, service, mapper, were deleted. All the tests were changed (#22)
    
    * spring profiles were created, school-admin url was moved to applicati… (#24)
    
    * spring profiles were created, school-admin url was moved to application.properties
    
    * import unused was removed
    
    * Implementing add and delete teacher from course (#23)
    
    * Implementing add and delete teacher from course
    
    * Add and Delete Teacher Tests in Connector Course were created
    
    * Add and Delete Teacher Test were implemented in Course Service
    
    * Add and Delete Teacher Test were implemented in Course Controller
    
    * library stringUtils was added
    
    * Changes in controller course test.
    
    * Changes in course tests
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    
    * courseId was removed
    
    * cors configuration was added
    
    Co-authored-by: Antonio Ezequiel Liendro <65373420+Tony-L-93@users.noreply.github.com>
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    Co-authored-by: Evelin Aragon <eve_aragon@hotmail.com>

---
+++ src/main/java/click/escuela/admin/core/configuration/ConfigurationApplication.java
@@ -0,0 +1,29 @@
+package click.escuela.admin.core.configuration;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties
+public class ConfigurationApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:4200","https://click-escuela-develop.herokuapp.com/","https://click-escuela-master.herokuapp.com/")
+					.allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
+					.maxAge(3600);
+
+			}
+
+		};
+	}
+}
+
