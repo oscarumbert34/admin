1f2119a6e22283755bcb2f125c74f1490a480915
Tony-L-93 <tonyliendro@gmail.com>
2021-06-18 21:20:18 -0300

    Save Excel was created and all its test too

--- src/main/java/click/escuela/admin/core/exception/TransactionException.java
+++ src/main/java/click/escuela/admin/core/exception/TransactionException.java
@@ -6,10 +6,14 @@ public class TransactionException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 5985148238792633272L;
-	private String code;
+	private final String code;
 	
 	public TransactionException(String code, String message) {
 		super(message);
 		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
 	}
 }
