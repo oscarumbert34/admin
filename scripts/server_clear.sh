ebdb03b46d4cbc21e02e8bb3fcdee1b07753f13f
Evelin Aragon <eve_aragon@hotmail.com>
2021-07-03 22:41:21 -0300

    deploy

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-rm -f /home/ec2-user/server/admin-core/stop.sh
-rm -f /home/ec2-user/server/admin-core/fix_privileges.sh
-rm -f /home/ec2-user/server/admin-core/server_clear.sh
+rm -rf /home/ec2-user/server/admin-core
