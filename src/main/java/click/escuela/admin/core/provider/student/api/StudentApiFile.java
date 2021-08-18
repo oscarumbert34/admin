773dbc8a62694315cc5fecfb1ffbd61a89bc2631
Tony-L-93 <tonyliendro@gmail.com>
2021-08-17 22:07:06 -0300

    Fixing coverage

--- src/main/java/click/escuela/admin/core/provider/student/api/StudentApiFile.java
+++ src/main/java/click/escuela/admin/core/provider/student/api/StudentApiFile.java
@@ -1,7 +1,6 @@
 package click.escuela.admin.core.provider.student.api;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +9,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class StudentApiFile extends StudentApi{
 
