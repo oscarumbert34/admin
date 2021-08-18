fc745c44974225574aa065aabdd4a209cbd46674
Tony-L-93 <tonyliendro@gmail.com>
2021-08-17 22:12:09 -0300

    Fixing coverage

--- src/main/java/click/escuela/admin/core/provider/student/dto/FileError.java
+++ src/main/java/click/escuela/admin/core/provider/student/dto/FileError.java
@@ -5,14 +5,9 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class FileError {
