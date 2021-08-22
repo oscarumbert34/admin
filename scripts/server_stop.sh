0661016fa3fe97516e3eb62ce762045365da85db
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-21 21:56:19 -0300

    actualizacion de scripts

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo kill -9 $(sudo lsof -t -i:8080)
+var="$(cat /home/ec2-user/server/admin-core/admin-service.pid)"
+sudo kill $var
+sudo rm -rf /home/ec2-user/server/admin-core/admin-service.pid
