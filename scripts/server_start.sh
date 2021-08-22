86c9da8804ee86458507a944a9a59033f575f574
Evelin Aragon <eve_aragon@hotmail.com>
2021-08-22 12:10:52 -0300

    fix de scripts

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/admin-core
+
+sudo rm -rf /home/ec2-user/server/admin-core/admin-service.pid
+
+echo"eliminando arcchivo"
+
 sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-e$
  admin-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > admin-service.pid
