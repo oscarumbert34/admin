3d07f19b17a8630c8a361de28d32a5b75828280c
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-08-24 21:21:49 -0300

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,5 +5,4 @@ sudo rm -rf /home/ec2-user/server/admin-core/admin-service.pid
 
 echo"eliminando arcchivo"
 
-sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-e$
- admin-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > admin-service.pid
+sudo java -jar admin-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > admin-service.pid
