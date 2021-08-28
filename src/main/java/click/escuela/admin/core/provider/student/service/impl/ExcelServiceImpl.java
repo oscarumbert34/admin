6a0d3a967f9052f38048e53fe8518791903dda6c
oscarumbert34 <oscar.umbert@gmail.com>
2021-08-28 20:48:34 -0300

    Develop (#43)
    * It configurated all to create Teacher (#13)
    
    * It configurated all to create Teacher
    
    * All the tests were created. And the packages are organized by each type
    
    * the url student was modified
    
    * the url student was modified
    
    * the database was changed to H2
    
    * Update teacher was created and all its tests (#15)
    
    * the swagger configuration was added (#18)
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    
    * feature/CE-265-listar-profesores (#20)
    
    * Get Teacher By Id and, Get Teachers by School Id and Course Id; were implented
    
    * All the tests were created
    
    * Code Smell fixed
    
    * Code Smell fixed
    
    * Another code Smell fixed
    
    * jacoco
    
    * the test was fixed
    
    * Get Bills by some params were implemented and also their tests (#21)
    
    * Entity School and its repository, service, mapper, were deleted. All the tests were changed (#22)
    
    * spring profiles were created, school-admin url was moved to applicati… (#24)
    
    * spring profiles were created, school-admin url was moved to application.properties
    
    * import unused was removed
    
    * Implementing add and delete teacher from course (#23)
    
    * Implementing add and delete teacher from course
    
    * Add and Delete Teacher Tests in Connector Course were created
    
    * Add and Delete Teacher Test were implemented in Course Service
    
    * Add and Delete Teacher Test were implemented in Course Controller
    
    * library stringUtils was added
    
    * Changes in controller course test.
    
    * Changes in course tests
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    
    * courseId was removed
    
    * cors configuration was added
    
    * Save Excel was created and all its test too (#27)
    
    * prueba deploy
    
    * deploy
    
    * deploy
    
    * deploy
    
    * mysql dependency was added
    
    * School Id was deleted from Student Body and all the student tests were changed (#28)
    
    * prueba
    
    * Add and Delete teacher from course were deleted and were added Add an… (#33)
    
    * Add and Delete teacher from course were deleted and were added Add and Delete courses from teacher. Also, all the tests were implemented
    
    * database conection was removed
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    
    * GenderType was added to TeacherDTO's body and schoolId was taken out of the teacherApi's body (#35)
    
    * Spring cloud (#36)
    
    * configure eureka
    
    * change url student
    
    * new url of angular was added
    
    * new url of angular was added
    
    * Feature/ce 324 cargar informacion de archivo en la base (#39)
    
    * interface bulkUpload and implement studentBulkUpload were created, apache poi library was added, StudentApiFile was created
    
    * Adding more fields in order to read the excel field
    
    * file was added
    
    * skip tittle was added
    
    * Adding more info into Excel to create the whole StudentApi
    
    * Now it's available to save a entire excel with Student and Parent. Also, the error are written on the sheet
    
    * Update pom.xml
    
    * Some test were corrected
    
    * error message was formated
    
    * Adding changes in ReadFile
    
    * Code smell fixed
    
    * Building more tests
    
    * Adding changes
    
    * test was fixed
    
    * Creating test for StudentBulkUpload
    
    * Fixing code smells
    
    * Fixing coverage
    
    * Fixing coverage
    
    * Fixing coverage
    
    * Fixing coverage
    
    * Fixing coverage
    
    * Fixing coverage
    
    * Adding changes
    
    * Adding changes
    
    * add comment
    
    * comments were added
    
    * refactor was done in StudentBulkUpload
    
    * index was changed
    
    Co-authored-by: umbert oscar <oscar.umbert@gmail.com>
    Co-authored-by: clickescuela <83097465+clickescuela@users.noreply.github.com>
    
    * properties was changed
    
    * profile prod and dev were deleted
    
    * cors was deleted
    
    * change multipart
    
    * fix test
    
    Co-authored-by: Antonio Ezequiel Liendro <65373420+Tony-L-93@users.noreply.github.com>
    Co-authored-by: clickescuela <83097465+clickescuela@users.noreply.github.com>
    Co-authored-by: Evelin Aragon <eve_aragon@hotmail.com>

--- src/main/java/click/escuela/admin/core/provider/student/service/impl/ExcelServiceImpl.java
+++ src/main/java/click/escuela/admin/core/provider/student/service/impl/ExcelServiceImpl.java
@@ -28,12 +28,23 @@ public class ExcelServiceImpl {
 	
 	@Async
 	public void save(String schoolId, MultipartFile excel) throws TransactionException, EncryptedDocumentException, IOException {
-		File file = new File(excel.getOriginalFilename());
+		
+		//File file = new File(excel.getOriginalFilename());
+		File file = multipartToFile(excel,"/excel");
 		List<StudentApiFile> students = studentBulkUpload.readFile(file);
 		List<FileError> errors = studentBulkUpload.upload(schoolId, students); 
 		studentBulkUpload.writeErrors(errors, file);
 		ExcelApi excelApi= ExcelApi.builder().name(file.getName()).schoolId(Integer.valueOf(schoolId)).file(file.getAbsolutePath()).studentCount(students.size()).build();
 		excelConnector.save(schoolId, excelApi);
+	}
+	
+	
+	private File multipartToFile(MultipartFile multipart, String fileName) throws IllegalStateException, IOException {
+		String listingFolder = System.getProperty("java.io.tmpdir");
+
+		File convFile = new File(listingFolder,fileName);
+	    multipart.transferTo(convFile);
+	    return convFile;
 	}
 	
 }
