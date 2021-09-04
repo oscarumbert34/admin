83b2e4c5cee2aef8925e699576bd31622c841631
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 17:36:07 -0300

    logs

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,4 +5,4 @@ sudo rm -rf /home/ec2-user/server/admin-core/admin-service.pid
 
 echo"eliminando arcchivo"
 
-sudo java -jar admin-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > admin-service.pid
+sudo java -jar -Dlogging.file.name=/home/ec2-user/server/admin-core/debug.log admin-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > admin-service.pid
