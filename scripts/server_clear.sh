f86ce3e08ec7e2bc9b6fca4b2cd4ff93ba1f80c7
Tu Nombre <eve_aragon@hotmail.com>
2021-10-13 13:42:31 -0300

    subir a produccion

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/admin-core
+sudo rm -rf /home/ec2-user/server/admin-core/appspec.yml
+sudo rm -rf /home/ec2-user/server/admin-core/admin-core-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/admin-core/admin-service.pid
+sudo rm -rf /home/ec2-user/server/admin-core/logs
+sudo rm -rf /home/ec2-user/server/admin-core/server_stop.sh
+sudo rm -rf /home/ec2-user/server/admin-core/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/admin-core/server_clear.sh
---
+++ scripts/server_prestart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /home/ec2-user/server/admin-core
+
+sudo sh server_start.sh
